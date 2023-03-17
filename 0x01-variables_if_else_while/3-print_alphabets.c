bea6dd53f4e7d65dac0a752a875104b17493ded8
adamin88 <elhihi.mohamed@gmail.com>
2023-03-17 06:26:13 -0700

    print lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char small_letter = 'a';
+while (small_letter <= 'z')
+{
+putchar(small_letter);
+small_letter++;
+}
+char big_letter = 'A';
+while (big_letter <= 'Z')
+{
+putchar(big_letter);
+big_letter++;
+putchar('\n');
+return (0);
+}
