6c315d12bf4f0f3fa9fd77fde1616a2cf5879fe1
adamin88 <elhihi.mohamed@gmail.com>
2023-03-16 19:00:01 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #inculde <stdio.h>
-
+/**
+ * print a text using puts function
+ * the return number should be 0 like this CODE=0
+ */
 int main(void)
 {
 	puts(""Programming is like building a multilingual puzzle\n");
