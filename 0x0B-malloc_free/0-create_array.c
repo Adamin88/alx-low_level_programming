3af97babe5a9b52419a2eba8bf55c97806deb9a7
adamin88 <elhihi.mohamed@gmail.com>
2023-04-11 18:08:27 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-if (size == 0)
-{
-return (NULL);
-}
-char *arr = (char *) malloc(size * sizeof(char));
-if (arr == NULL)
-{
-return (NULL);
-}
-for (unsigned int i = 0; i < size; i++)
-{
-arr[i] = c;
-}
-return (arr);
+	char *str;
+	unsigned int i;
+
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
