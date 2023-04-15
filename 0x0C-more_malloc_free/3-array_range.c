a46345efdc3ed5cabcafd2a37d6d9b01a6fd020d
adamin88 <elhihi.mohamed@gmail.com>
2023-04-14 18:15:57 -0700

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,46 +1,25 @@
-#include <stdlib.h>
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * _memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- *
- * Return: pointer to the memory area s
+ *array_range - A function that creates an array of integers
+ *@min: a variable that takes in a number as the argument
+ *@max: a variable that takes in the max number as the argument
+ *The output will output up to and including the max value.
+ *Return: Pointer will point to the newly created array.
  */
-
-char *_memset(char *s, char b, unsigned int n)
+int *array_range(int min, int max)
 {
-	unsigned int i;
+	int i, *p;
 
-	for (i = 0; i < n; i++)
+	if (min > max)
+		return (NULL);
+	p = malloc((max - min + 1) * sizeof(int));
+	i = 0;
+	while (min <= max)
 	{
-		s[i] = b;
+		p[i] = min;
+		min++;
+		i++;
 	}
-
-	return (s);
-}
-
-/**
- * _calloc - allocates memory for an array
- * @nmemb: number of elements in the array
- * @size: size of each element
- *
- * Return: pointer to allocated memory
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	char *ptr;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	ptr = malloc(size * nmemb);
-
-	if (ptr == NULL)
-		return (NULL);
-
-	_memset(ptr, 0, nmemb * size);
-
-	return (ptr);
+	return (p);
 }
