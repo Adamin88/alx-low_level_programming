81ed8abc78e3bdb860060d653b8829d650befe69
adamin88 <elhihi.mohamed@gmail.com>
2023-05-08 19:52:15 -0700

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,11 +4,11 @@
 #define SE STDERR_FILENO
 
 /**
- * main - create the copy bash script
- * @ac: argument count
- * @av: arguments as strings
+ * main - entry point for creating a copy of file_from as file_to
+ * @ac: count of arguments
+ * @av: array of arguments
  *
- * Return: 0 on success, other value on failure
+ * Return: 0 on success, otherwise exit status code
  */
 int main(int ac, char *av[])
 {
@@ -17,63 +17,37 @@ int main(int ac, char *av[])
 	mode_t mode;
 
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-
 	if (ac != 3)
-	{
-		dprintf(SE, "Usage: cp file_from file_to\n");
-		return (97);
-	}
+		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
 
 	input_fd = open(av[1], O_RDONLY);
 	if (input_fd == -1)
-	{
-		dprintf(SE, "Error: Can't read from file %s\n", av[1]);
-		return (98);
-	}
+		dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	output_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (output_fd == -1)
-	{
-		dprintf(SE, "Error: Can't write to %s\n", av[2]);
-		close(input_fd);
-		return (99);
-	}
+		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
 
 	do {
 		istatus = read(input_fd, buf, MAXSIZE);
 		if (istatus == -1)
-		{
-			dprintf(SE, "Error: Can't read from file %s\n", av[1]);
-			close(input_fd);
-			close(output_fd);
-			return (98);
-		}
+			dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 		if (istatus > 0)
 		{
 			ostatus = write(output_fd, buf, (ssize_t) istatus);
 			if (ostatus == -1)
-			{
-				dprintf(SE, "Error: Can't write to %s\n", av[2]);
-				close(input_fd);
-				close(output_fd);
-				return (99);
-			}
+				dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
 	} while (istatus > 0);
 
 	istatus = close(input_fd);
 	if (istatus == -1)
-	{
-		dprintf(SE, "Error: Can't close fd %d\n", input_fd);
-		return (100);
-	}
+		dprintf(SE, "Error: Can't close fd %d\n", input_fd), exit(100);
 
 	ostatus = close(output_fd);
 	if (ostatus == -1)
-	{
-		dprintf(SE, "Error: Can't close fd %d\n", output_fd);
-		return (100);
-	}
+		dprintf(SE, "Error: Can't close fd %d\n", output_fd), exit(100);
 
 	return (0);
 }
